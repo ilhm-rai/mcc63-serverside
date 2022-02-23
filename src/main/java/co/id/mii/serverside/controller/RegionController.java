c9dbf40eee5d9f435c18597937de885b7238e8f8
joahanW <47098275+joahanW@users.noreply.github.com>
2022-02-23 16:24:40 +0800

    CRUD Region

---
+++ src/main/java/co/id/mii/serverside/controller/RegionController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.serverside.controller;
+
+import co.id.mii.serverside.model.Region;
+import co.id.mii.serverside.service.RegionService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author MSI-JO
+ */
+@RestController
+@RequestMapping("/region")
+public class RegionController {
+    
+    private RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+    
+    @GetMapping
+    public List<Region> getAll() {
+      return regionService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Region getById(@PathVariable Long id) {
+        return regionService.getById(id);
+    }
+
+    @PostMapping
+    public Region create(@RequestBody Region region) {
+        return regionService.create(region);
+    }
+
+    @PutMapping("/{id}")
+    public Region update(@PathVariable Long id, @RequestBody Region region) {
+       return regionService.update(id, region);
+    }
+
+    @DeleteMapping("/{id}")
+    public Region delete(@PathVariable Long id) {
+        return regionService.delete(id);
+    }
+}
+
+/**
+ * localhost:8088/api/region/
+ * localhost:8088/api/region/
+ * 
+ * localhost:8088/api/region/detail?id=10 11111
+ * localhost:8088/api/region/10 GET
+ * localhost:8088/api/region/10 PUT
+ */
---
+++ src/test/java/co/id/mii/serverside/Mcc63ServersideApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.mii.serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcc63ServersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
