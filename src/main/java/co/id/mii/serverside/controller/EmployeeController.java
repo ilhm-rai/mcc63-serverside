468872e76ce9645f811122685942aacdc7a1ffad
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-02-26 01:55:11 +0700

    Add request create, getEmployees, and delete

--- src/main/java/co/id/mii/serverside/controller/EmployeeController.java
+++ src/main/java/co/id/mii/serverside/controller/EmployeeController.java
@@ -6,12 +6,24 @@
 package co.id.mii.serverside.controller;
 
 import co.id.mii.serverside.model.Employee;
-import co.id.mii.serverside.model.dto.EmployeeData;
+import co.id.mii.serverside.model.dto.EmployeeDto;
 import co.id.mii.serverside.service.EmployeeService;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,17 +33,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    
-    private EmployeeService employeeService;
+
+    private final EmployeeService employeeService;
+
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService, ModelMapper modelMapper) {
         this.employeeService = employeeService;
+        this.modelMapper = modelMapper;
     }
-    
+
+    @GetMapping
+    @ResponseBody
+    public List<EmployeeDto> getEmployees() {
+        List<Employee> employee = employeeService.getEmployeesList();
+        return employee.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
     @PostMapping
-    public Employee create(@RequestBody EmployeeData employeeData){
-        return employeeService.create(employeeData);
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public EmployeeDto create(@RequestBody EmployeeDto employeeDto) throws ParseException {
+        if (employeeDto.getId() != null) {
+            employeeDto.setId(null);
+        }
+        Employee employee = convertToEntity(employeeDto);
+        Employee employeeCreated = employeeService.create(employee);
+        return convertToDto(employeeCreated);
     }
     
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String delete(@PathVariable("id") Long id) {
+        employeeService.delete(id);
+        return "Employee deleted.";
+    }
+
+    private EmployeeDto convertToDto(Employee employee) {
+        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+        return employeeDto;
+    }
+
+    private Employee convertToEntity(EmployeeDto employeeDto) throws ParseException {
+        Employee employee = modelMapper.map(employeeDto, Employee.class);
+        return employee;
+    }
 }
