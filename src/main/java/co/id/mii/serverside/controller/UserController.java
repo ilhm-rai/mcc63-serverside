c62301c1ef1bd5e548569d0eb5ba6319ec97291f
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-03-01 11:36:09 +0700

    Create user controller

---
+++ src/main/java/co/id/mii/serverside/controller/UserController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.serverside.controller;
+
+import co.id.mii.serverside.model.User;
+import co.id.mii.serverside.service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author RAI
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    private final UserService userService;
+    
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @GetMapping
+    @ResponseBody
+    public List<User> getUsers() {
+        return userService.getAll();
+    }
+    
+    @GetMapping("/{id}")
+    @ResponseBody
+    public User getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+    
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public User delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+}
