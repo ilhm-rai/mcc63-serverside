1a8ffbc21e8be9e21a0603b91a1326fd27cebf51
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-03-01 11:35:45 +0700

    Create user service

---
+++ src/main/java/co/id/mii/serverside/service/UserService.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.serverside.service;
+
+import co.id.mii.serverside.model.User;
+import co.id.mii.serverside.model.dto.EmployeeDto;
+import co.id.mii.serverside.repository.UserRepository;
+import java.text.ParseException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ *
+ * @author RAI
+ */
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    private final EmployeeService employeeService;
+    
+    @Autowired
+    public UserService(UserRepository userRepository, EmployeeService employeeService) {
+        this.userRepository = userRepository;
+        this.employeeService = employeeService;
+    }
+    
+    public EmployeeDto create(EmployeeDto employeeDto) throws ParseException {
+       return employeeService.create(employeeDto);
+    }
+    
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+    
+    public User getById(Long id) {
+        return userRepository.findById(id).orElseThrow(()
+                -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role Not Found"));
+    }
+    
+    public User update(Long id, User user) {
+        User u = getById(id);
+        user.setId(id);
+        user.setEmployee(u.getEmployee());
+        user.setRoles(u.getRoles());
+        return userRepository.save(user);
+    }
+    
+    public User delete(Long id) {
+        User user = getById(id);
+        userRepository.delete(user);
+        return user;
+    }
+}
